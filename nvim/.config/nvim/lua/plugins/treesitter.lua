3d582fbcd4c0e06f0d755cee48aee9bcc254a86e
ehjc1 <eugenechew2012@gmail.com>
2026-03-04 14:36:18 +1300

    update/nvim: add additional parsers

---
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,12 @@
+return {
+  -- add more treesitter parsers
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "css",
+        "scss",
+      },
+    },
+  },
+}
