aa7148d6ea0f1a4a86bc15904facef5026c97b3c
ehjc1 <eugenechew2012@gmail.com>
2026-07-29 09:04:53 +1200

    fix(nvim): remove dynamic file explorer and use cwd as file explorer for neo-tree

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Pin root detection to cwd so multi-repo workspaces don't jump to the LSP/git
+-- root of whatever file is open
+vim.g.root_spec = { "cwd" }
